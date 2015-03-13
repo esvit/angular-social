85ab25d7bd4487cf5a38562a05c82741880ae8ae
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-03-13 13:00:14 +0100

    Add linkedin support

--- angular-social.src.js
+++ angular-social.src.js
@@ -645,6 +645,58 @@ app.directive('ngSocialMoiKrug', ['$parse', function ($parse) {
     };
 }]);
 
+app.directive('ngSocialLinkedin', function() {
+    'use strict';
+
+    var options = {
+        counter: {
+            url: '//www.linkedin.com/countserv/count/share?url={url}&format=jsonp&callback=JSON_CALLBACK',
+            getNumber: function(data) {
+                return data.count;
+            }
+        },
+        popup: {
+            url: 'http://www.linkedin.com/shareArticle?mini=true&url={url}&title={title}&summary={description}',
+            width: 600,
+            height: 450
+        },
+        click: function(options) {
+            // Add colon to improve readability
+            if (!/[\.:\-–—]\s*$/.test(options.pageTitle)) options.pageTitle += ':';
+            return true;
+        },
+        track: {
+            'name': 'LinkedIn',
+            'action': 'share'
+        }
+    };
+    return {
+        restrict: 'C',
+        require: '^?ngSocialButtons',
+        scope: true,
+        replace: true,
+        transclude: true,
+        template: '<li> \
+                    <a ng-href="{{ctrl.link(options)}}" target="_blank" ng-click="ctrl.clickShare($event, options)" class="ng-social-button"> \
+                        <span class="ng-social-icon"></span> \
+                        <span class="ng-social-text" ng-transclude></span> \
+                    </a> \
+                    <span ng-show="count" class="ng-social-counter">{{ count }}</span> \
+                   </li>',
+        controller: function($scope) {
+        },
+        link: function(scope, element, attrs, ctrl) {
+            element.addClass('ng-social-linkedin');
+            if (!ctrl) {
+                return;
+            }
+            scope.options = options;
+            scope.ctrl = ctrl;
+            ctrl.init(scope, element, options);
+        }
+    }
+});
+
 angular.module('ngSocial').run(['$templateCache', function ($templateCache) {
 	$templateCache.put('/views/buttons.html', '<div class="ng-social-container ng-cloak"><ul class="ng-social" ng-transclude></ul></div>');
 }]);
